3bfceb38eca3ec24813d38908691c0aca8a01e06
meraj2704 <mh1669101@gmail.com>
2025-12-28 23:49:39 +0600

    updated code

--- src/app/(main)/(auth)/login/page.tsx
+++ src/app/(main)/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginPage from "@/src/components/auth/LoginPage";
+import { Suspense } from "react";
 
 export default function page() {
   return (
     <>
+     <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
       <LoginPage />
+    </Suspense>
     </>
   );
 }
